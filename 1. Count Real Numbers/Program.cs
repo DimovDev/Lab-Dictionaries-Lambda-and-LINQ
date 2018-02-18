75c082a43e50acf348421223acf45469e980159b
sapa73 <34308956+sapa73@users.noreply.github.com>
2018-02-18 15:43:20 +0200

    new

---
+++ 1. Count Real Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace _1._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string[] numbers = Console.ReadLine().Split(' ');
+            var counts = new SortedDictionary<double, int>();
+            foreach (var item in numbers)
+            {
+                double num = double.Parse(item);
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+            foreach (var item in counts)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
---
+++ 2. Odd Occurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().ToLower().Split(' ');
+            var counts = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                
+                if (counts.ContainsKey(word))
+                {
+                    counts[word]++;
+                }
+                else
+                {
+                    counts[word] = 1;
+                }
+            }
+            var result = new List<string>();
+            foreach (var item in counts )
+            {
+                if (item.Value % 2 !=0)
+                {
+                    result.Add(item.Key);
+                }
+            }
+            Console.WriteLine(string.Join(", ",result));
+        }
+    }
+}
